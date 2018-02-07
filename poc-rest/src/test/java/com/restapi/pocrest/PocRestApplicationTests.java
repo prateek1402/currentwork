9df97d3619e1948a1af6ee13d601194414e73ea6
prateek1402 <prateek1402@users.noreply.github.com>
2018-02-07 12:23:48 +0530

    Update PocRestApplicationTests.java

--- poc-rest/src/test/java/com/restapi/pocrest/PocRestApplicationTests.java
+++ poc-rest/src/test/java/com/restapi/pocrest/PocRestApplicationTests.java
@@ -1,16 +1,16 @@
 package com.restapi.pocrest;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class PocRestApplicationTests {
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.restapi")
+public class PocRestApplication {
 
-	@Test
-	public void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(PocRestApplication.class, args);
 	}
-
 }
