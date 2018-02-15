e205d6c57c906d1042bff3d5bf7b6e4fc06afb3e
prateek1402 <prateek1402@users.noreply.github.com>
2018-02-15 16:55:11 +0530

    Update PersonController.java
    up1

--- poc-rest/src/main/java/com/restapi/controller/PersonController.java
+++ poc-rest/src/main/java/com/restapi/controller/PersonController.java
@@ -4,19 +4,36 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.restapi.dao.PersonRepository;
 import com.restapi.model.Person;
 import com.restapi.service.PersonService;
 
 @RestController
-@RequestMapping("/rest/persons")
+@RequestMapping("/restapi")
 public class PersonController {
 	@Autowired
 	private PersonService personService;
+	
+	@Autowired PersonRepository pr;
+	
 	@GetMapping("/all")
     public List<Person> getAll() {
         return personService.getAllPersonInfo();
+    }
+	@RequestMapping(method=RequestMethod.POST, value="/createperson")
+    public String addPerson(@RequestBody Person person) {
+		
+		
+		
+     if(   pr.save(person)!=null)
+    	 return "lol";
+     else
+    	 return "lol2"; 
+        
     }
 }
