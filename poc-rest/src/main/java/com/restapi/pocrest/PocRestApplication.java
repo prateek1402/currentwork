c55b5576b9221a7774a28f7792131fa747ae13c7
prateek1402 <prateek1402@users.noreply.github.com>
2018-02-15 16:52:13 +0530

    Update PocRestApplication.java

--- poc-rest/src/main/java/com/restapi/pocrest/PocRestApplication.java
+++ poc-rest/src/main/java/com/restapi/pocrest/PocRestApplication.java
@@ -1,9 +1,13 @@
 package com.restapi.pocrest;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.restapi")
 public class PocRestApplication {
 
 	public static void main(String[] args) {
