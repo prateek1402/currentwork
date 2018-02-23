8633000986ae75206b3ffdb095aeab18da0d6258
prateek1402 <prateek1402@users.noreply.github.com>
2018-02-23 16:31:21 +0530

    Update PocRestApplication.java
    u1

--- poc-rest/src/main/java/com/restapi/pocrest/PocRestApplication.java
+++ poc-rest/src/main/java/com/restapi/pocrest/PocRestApplication.java
@@ -3,13 +3,20 @@ package com.restapi.pocrest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan("com.restapi")
 public class PocRestApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PocRestApplication.class, args);
 	}
